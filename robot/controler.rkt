9011aff66c566b2f9fd082c805080392e5945b17
Cecília Carneiro e Silva <cecilia.carneiroesilva@gmail.com>
2017-07-10 20:52:12 -0300

    demo forget

--- robot/controler.rkt
+++ robot/controler.rkt
@@ -3,8 +3,14 @@
 (require "robo.scm")
 (require "dh.rkt")
 
+(provide demo)
+
 (define (demo x y z)
   (displayln "Cinematica Inversa")
+  (cinematica-inversa 'x 'forget)
+  (cinematica-inversa 'y 'forget)
+  (cinematica-inversa 'z 'forget)
+  
   (cinematica-inversa 'x 'set x)
   (cinematica-inversa 'y 'set y)
   (cinematica-inversa 'z 'set z)
